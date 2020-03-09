02aa9e645f2a52cba21ef7feff02c1cc7232a38c
Craig Threadgold <threadgoldc@gmail.com>
2020-03-09 11:39:52 +0100

    added lambda version update

--- scripts/deploy_lambda_package.sh
+++ scripts/deploy_lambda_package.sh
@@ -4,6 +4,8 @@ do
     LAMBDA_PATH=$PWD/lambda/$LAMBDA_NAME
     cd $LAMBDA_PATH
 
+    GIT_HASH=$(git rev-parse HEAD | head -c 10)
+
     rm -rf venv
     python3 -m venv venv
     ./venv/bin/pip3 install --no-cache-dir -r requirements.txt
@@ -11,7 +13,9 @@ do
     zip -r9 ./function.zip .
     zip -g function.zip function.py
 
-    aws s3 cp function.zip s3://dev-s3-lambda-deployment/$LAMBDA_NAME/function.zip
+    aws s3 cp function.zip s3://dev-s3-lambda-deployment/$LAMBDA_NAME/$GIT_HASH.zip
+
+    python3 ../../scripts/update_config.py $GIT_HASH
 
     rm -rf venv
     rm function.zip
