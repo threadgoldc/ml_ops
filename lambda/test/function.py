c4cb0a6645023a7d27e64d74db85f987316d389e
Craig Threadgold <threadgoldc@gmail.com>
2020-03-09 13:39:27 +0100

    lambda code change test

--- lambda/test/function.py
+++ lambda/test/function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO banter
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda cuz!')
+        'body': json.dumps('Hello from Lambda fam!')
     }
