8ed4c9350133d4b8b3c0439b76d3544a0e9f9a00
Craig Threadgold <threadgoldc@gmail.com>
2020-03-12 16:51:08 +0100

    pipeline webhook test

--- lambda/test/function.py
+++ lambda/test/function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO banter
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda')
+        'body': json.dumps('Hello from Lambda!')
     }
