de8472d465dc596850b46fb2caa04b855f506c06
Craig Threadgold <threadgoldc@gmail.com>
2020-03-13 19:07:50 +0100

    undo my hacking about

--- scripts/create-stack.sh
+++ scripts/create-stack.sh
@@ -3,5 +3,5 @@ STACK_NAME=$1
 aws cloudformation create-stack  \
     --stack-name $STACK_NAME  \
     --template-body file://cf-templates/$STACK_NAME.yml \
-    --parameters file://cf-templates-params/temp.json \
+    --parameters file://cf-templates-params/$STACK_NAME.json \
     --capabilities CAPABILITY_NAMED_IAM
--- scripts/update-stack.sh
+++ scripts/update-stack.sh
@@ -3,5 +3,5 @@ STACK_NAME=$1
 aws cloudformation update-stack  \
     --stack-name craig-$STACK_NAME  \
     --template-body file://cf-templates/$STACK_NAME.yml \
-    --parameters file://cf-templates-params/temp.json \
+    --parameters file://cf-templates-params/$STACK_NAME.json \
     --capabilities CAPABILITY_NAMED_IAM
