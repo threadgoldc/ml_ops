16cfd0c7b9735605979c2d22e866f92fbaa6aa70
Craig Threadgold <threadgoldc@gmail.com>
2020-03-13 18:14:46 +0100

    add training job done for lambda fix 3

--- lambda/test/function.py
+++ lambda/test/function.py
@@ -9,17 +9,16 @@ def lambda_handler(event, context):
 
     try:
     
-    codepipeline.put_job_success_result(jobId=jobId)
+        codepipeline.put_job_success_result(jobId=jobId)
 
     except Exception as e:
-                  print(e)
-                  resp = codepipeline.put_job_failure_result(
-                      jobId=jobId,
-                      failureDetails={
-                          'type': 'ConfigurationError',
-                          'message': str(e),
-                          'externalExecutionId': context.aws_request_id
-                      }
-                  )
+        print(e)
+        resp = codepipeline.put_job_failure_result(jobId=jobId,
+                                                    failureDetails={
+                                                        'type': 'ConfigurationError',
+                                                        'message': str(e),
+                                                        'externalExecutionId': context.aws_request_id
+                                                    }
+                                                )
 
     print('donzos')
