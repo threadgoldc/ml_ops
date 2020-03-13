13a967c606ba1d1c06e3054f4f8835fa3be3ac39
Craig Threadgold <threadgoldc@gmail.com>
2020-03-13 15:40:28 +0100

    mlops pipeline creation

--- scripts/update-stack.sh
+++ scripts/update-stack.sh
@@ -1,7 +1,7 @@
 STACK_NAME=$1
 
-aws cloudformation update-stack  \
+aws cloudformation create-stack  \
     --stack-name $STACK_NAME  \
     --template-body file://cf-templates/$STACK_NAME.yml \
-    --parameters file://cf-templates-params/$STACK_NAME.json \
+    --parameters file://cf-templates-params/temp.json \
     --capabilities CAPABILITY_NAMED_IAM
