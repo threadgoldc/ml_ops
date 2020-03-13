b8ea502d457c460f3c1a690e2c2d3da90b44759c
Craig Threadgold <threadgoldc@gmail.com>
2020-03-13 17:30:50 +0100

    update print in lambda

--- scripts/update-stack.sh
+++ scripts/update-stack.sh
@@ -1,7 +1,7 @@
 STACK_NAME=$1
 
-aws cloudformation create-stack  \
-    --stack-name $STACK_NAME  \
+aws cloudformation update-stack  \
+    --stack-name craig-$STACK_NAME  \
     --template-body file://cf-templates/$STACK_NAME.yml \
     --parameters file://cf-templates-params/temp.json \
     --capabilities CAPABILITY_NAMED_IAM
