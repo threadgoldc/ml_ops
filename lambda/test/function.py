c652b507dd953730013b3ef41fc178b58d9c5f67
Craig Threadgold <threadgoldc@gmail.com>
2020-03-13 18:11:12 +0100

    add training job done for lambda fix 2

--- lambda/test/function.py
+++ lambda/test/function.py
@@ -3,36 +3,23 @@ import json
 def lambda_handler(event, context):
     import boto3
 
-    codepipeline = boto3.client('codepipeline')
 
-    pipeline_name = '${AWS::StackName}'
-    token = None
+    codepipeline = boto3.client('codepipeline')
+    jobId = event["CodePipeline.job"]["id"]
 
     try:
-        response = codepipeline.get_pipeline_state( name=pipeline_name )
-        executionId = response['stageStates'][0]['latestExecution']['pipelineExecutionId']
-        
-        # Get the approve train status token
-        for stageState in response['stageStates']:
-            if stageState['stageName'] == 'TrainApproval':
-                for actionState in stageState['actionStates']:
-                    if actionState['actionName'] == 'ApproveTrain':
-                        latestExecution = actionState['latestExecution']
-                        if latestExecution['status'] != 'InProgress':
-                            raise(Exception("Train approval is not awaiting for approval: %s" % latestExecution['status']))
-                    token = latestExecution['token']
+    
+    codepipeline.put_job_success_result(jobId=jobId)
 
-        if token is None:
-            raise(Exception("Action token wasn't found. Aborting..."))
+    except Exception as e:
+                  print(e)
+                  resp = codepipeline.put_job_failure_result(
+                      jobId=jobId,
+                      failureDetails={
+                          'type': 'ConfigurationError',
+                          'message': str(e),
+                          'externalExecutionId': context.aws_request_id
+                      }
+                  )
 
-        codepipeline.put_approval_result(
-              pipelineName=pipeline_name,
-              stageName='TrainApproval',
-              actionName='ApproveTrain',
-              token=token
-            )
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    print('donzos')
