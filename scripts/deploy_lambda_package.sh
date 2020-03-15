9321fca5f0bfbef595d51eb636da7a3465237d50
Craig Threadgold <threadgoldc@gmail.com>
2020-03-15 17:15:38 +0000

    git clone test 10

--- scripts/deploy_lambda_package.sh
+++ scripts/deploy_lambda_package.sh
@@ -1,28 +1,28 @@
-# for dir in ./lambda/*/  
-# do
-#     LAMBDA_NAME=$(basename "$dir")
-#     LAMBDA_PATH=$PWD/lambda/$LAMBDA_NAME
-#     cd $LAMBDA_PATH
+for dir in ./lambda/*/  
+do
+    LAMBDA_NAME=$(basename "$dir")
+    LAMBDA_PATH=$PWD/lambda/$LAMBDA_NAME
+    cd $LAMBDA_PATH
 
-#     rm -rf venv
-#     python3 -m venv venv
-#     ./venv/bin/pip3 install --no-cache-dir -r requirements.txt
+    rm -rf venv
+    python3 -m venv venv
+    ./venv/bin/pip3 install --no-cache-dir -r requirements.txt
 
-#     zip -r9 ./function.zip .
-#     zip -g function.zip function.py
+    zip -r9 ./function.zip .
+    zip -g function.zip function.py
 
-#     USERNAME_OUTPUT=$(aws cloudformation describe-stacks --stack-name pipeline --query "Stacks[0].Outputs[?OutputKey=='UsernameOutput'].OutputValue" --output text)
+    USERNAME_OUTPUT=$(aws cloudformation describe-stacks --stack-name pipeline --query "Stacks[0].Outputs[?OutputKey=='UsernameOutput'].OutputValue" --output text)
 
-#     aws s3 cp function.zip s3://$USERNAME_OUTPUT-s3-lambda-deployment/$LAMBDA_NAME/$CODEBUILD_BUILD_ID.zip
+    aws s3 cp function.zip s3://$USERNAME_OUTPUT-s3-lambda-deployment/$LAMBDA_NAME/$CODEBUILD_BUILD_ID.zip
 
-#     rm -rf venv
-#     rm function.zip
+    rm -rf venv
+    rm function.zip
 
-#     cd ../..
+    cd ../..
     
-# done
+done
 
-# python3 ./scripts/update_config.py $CODEBUILD_BUILD_ID
+python3 ./scripts/update_config.py $CODEBUILD_BUILD_ID
 
 TOKEN=$(aws ssm get-parameter --name github --query Parameter.Value)
 GITHUB_TOKEN=${TOKEN//[^[:alnum:]]/}
