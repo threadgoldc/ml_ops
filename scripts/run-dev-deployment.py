2656c16cf6a2ab335ec9cd11f7c79065bce2bec2
Craig Threadgold <threadgoldc@gmail.com>
2020-03-18 17:38:41 +0000

    branch name parameter update

--- scripts/run-dev-deployment.py
+++ scripts/run-dev-deployment.py
@@ -11,13 +11,6 @@ def update_config(git_branch):
     with open('./cf-templates-params/uva-dev-pipeline.json', 'w') as outfile:
         json.dump(data, outfile)
 
-    with open('./cf-templates-params/dev/mlops-resources.json') as json_file:
-        data = json.load(json_file)
-        data['Parameters']['GitBranchName'] = git_branch
-
-    with open('./cf-templates-params/dev/mlops-resources.json', 'w') as outfile:
-        json.dump(data, outfile)
-
 if __name__ == '__main__':
     git_branch = argv[1]
     update_config(git_branch)
