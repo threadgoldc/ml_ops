47c6636cd681a5801fd3adabd685a8e47ad60fbc
Craig Threadgold <threadgoldc@gmail.com>
2020-03-19 14:34:02 +0000

    test staging deploy

--- scripts/main-pipeline-run.sh
+++ scripts/main-pipeline-run.sh
@@ -1,17 +1,9 @@
-COMMIT_MESSAGE=$1
-
 GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-git add .
-
-git commit -m $COMMIT_MESSAGE
-
-git push --set-upstream origin $GIT_BRANCH
 
 git checkout staging
 
 git pull
 
-git merge $GIT_BRANCH -m $COMMIT_MESSAGE
+git merge $GIT_BRANCH -m $(git log -1 --pretty=%B)
 
 git push
