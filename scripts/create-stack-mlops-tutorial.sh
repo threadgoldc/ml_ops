9e63826901fd7b90609b9da326936c1ac1377ebf
Floris Calkoen <floris_calkoen@hotmail.com>
2020-03-23 18:52:43 +0100

    basic nb instance

--- scripts/create-stack-mlops-tutorial.sh
+++
@@ -1,5 +0,0 @@
-aws cloudformation create-stack  \
-    --stack-name model-hosting-stack  \
-    --template-body file://cf-templates/model-hosting.yml \
-    --parameters file://cf-templates-params/model-hosting.json \
-    --capabilities CAPABILITY_NAMED_IAM
