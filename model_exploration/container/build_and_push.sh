a04c95da57c77da61a965f2a3fa3c23e980dfefc
Bonaventura Pacileo <bonaventurapacileo@gmail.com>
2020-03-27 20:27:29 +0100

    model exploration update

--- model_exploration/container/build_and_push.sh
+++ model_exploration/container/build_and_push.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-# This script shows how to build the Docker image and push it to ECR to be ready for use
-# by SageMaker.
+
 
 # The argument to this script is the image name. This will be used as the image on the local
 # machine and combined with the account and region to form the repository name for ECR.
@@ -48,6 +47,7 @@ $(aws ecr get-login --region ${region} --no-include-email)
 # with the full name.
 
 docker build  -t ${image} .
+
 docker tag ${image} ${fullname}
 
 docker push ${fullname}
